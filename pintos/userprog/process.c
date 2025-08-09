059cd1563715a40e0176dac9a6c804574aa1e8aa
Jiyun Lee <kuri12071112@gmail.com>
2025-08-09 17:34:43 +0900

    setup_stack 구현

--- pintos/userprog/process.c
+++ pintos/userprog/process.c
@@ -857,10 +857,14 @@ static bool setup_stack(struct intr_frame *if_) {
      * TODO: You should mark the page is stack. */
     /* TODO: Your code goes here */
 
-    success = vm_claim_page(stack_bottom);
-    if (success) {
-        if_->rsp = USER_STACK;
-        thread_current()->stack_bottom = stack_bottom;
+    /* 페이지 할당이 성공한다면 claim_page 하고, rsp가 유저 스택의 밑을 가르키게 만듭니다.*/
+    if (vm_alloc_page_with_initializer(VM_ANON | VM_MARKER_0, stack_bottom, true, NULL, NULL)) {
+        success = vm_claim_page(stack_bottom);
+
+        if (success) {
+            if_->rsp = USER_STACK;
+            thread_current()->stack_bottom = stack_bottom;
+        }
     }
 
     return success;
